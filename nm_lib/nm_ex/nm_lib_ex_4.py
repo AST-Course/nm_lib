d4aaf87df6a6ecae95adadc2a92e0e6dc54edf6d
Juan <jumasy1980@gmail.com>
2025-01-24 01:20:37 -0800

    [ADD] Lax and minor typos

--- nm_lib/nm_ex/nm_lib_ex_4.py
+++ nm_lib/nm_ex/nm_lib_ex_4.py
@@ -10,6 +10,59 @@ import numpy as np
 from nm_lib.nm_ex.nm_lib_ex_1 import deriv_fwd
 
 
+
+def evolv_Lax_adv_burgers(
+    xx: np.ndarray,
+    hh: np.ndarray,
+    nt: int,
+    a: np.ndarray,
+    cfl_cut: float = 0.98,
+    ddx = lambda x,y: deriv_fwd(x, y),
+    bnd_type: str='wrap',
+    bnd_limits: list = [0,1],
+    **kwargs,
+):
+    r"""
+    Advance nt time-steps in time the burger eq for a being a fix constant or array.
+
+    Requires
+    --------
+    step_adv_burgers
+
+    Parameters
+    ----------
+    xx : `array`
+        Spatial axis.
+    hh : `array`
+        A function that depends on xx.
+    nt : `int`
+        Number of iterations
+    a : `float` or `array`
+        Either constant or array multiplies the right-hand side of the Burger's eq.
+    cfl_cut : `float`
+        Constant value to limit dt from cfl_adv_burger.
+        By default 0.98
+    ddx : `lambda function`
+        Allows to change the space derivative function.
+        By default lambda x,y: deriv_fwd(x, y)
+    bnd_type : `string`
+        It allows one to select the type of boundaries.
+        By default, 'wrap'
+    bnd_limits : `list(int)`
+        Array of two integer elements. The number of pixels that
+        will need to be updated with the boundary information.
+        By default [0,1]
+
+    Returns
+    -------
+    t : `array`
+        Time 1D array
+    unnt : `array`
+        Spatial and time evolution of u^n_j for n = (0,nt), and where j represents
+        all the elements of the domain.
+    """
+
+
 def evolv_Lax_uadv_burgers(
     xx: np.ndarray,
     hh: np.ndarray,
@@ -72,7 +125,7 @@ def Rie_flux(
     Returns
      -------
      flux : `array`
-         h^2
+         h^2/2
     """
 
 
@@ -100,9 +153,10 @@ def Rie_va(
 def Rie_interface_flux(
     uL: np.ndarray,
     uR: np.ndarray,
+    va: np.ndarray,
 ):
     """
-     absolute propagating speed (va). Uses Rie_va and Rie_flux
+     Interface Rusanov flux
 
      Parameters
      ----------
@@ -110,6 +164,8 @@ def Rie_interface_flux(
          Left and side variable
      uR: `array`
          Right and side variable
+     va : `array`
+         absolute va speed
 
     Returns
      -------
